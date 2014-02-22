0fdda2a7a12b26bac9a9c807c4c17c8b43dbbbd4
Ubuntu <ubuntu@ip-172-31-19-79.us-west-2.compute.internal>
2014-02-22 00:24:23 +0000

    added modified versionter the commit message for your changes. Lines starting

--- server_test3.js
+++ server_test3.js
@@ -1,8 +1,3 @@
-var databaseUrl = "BllipRatings";
-var mongojs = require('mongojs');
-var db = mongojs(databaseUrl);
-
-
 var http = require("http");
 var url = require("url");
 var querystring = require("querystring");
@@ -11,25 +6,29 @@ function start(route) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		var query = url.parse(request.url).query;				
-		var aWineID = String("a" + query);
+		var aWineID = "a" + query;
 		console.log("Request for " + pathname + " received.");
 		console.log(query + "   " + aWineID);	
 		route(pathname);
 
 		
-
-		var mycollection = db.collection(aWineID);
-		mycollection.find({avg: 1}, {_id: 0}, function(err, aWineID){
-			if( err || !aWineID) console.log("No average found");
-			else aWineID.forEach( function(OutputAvg) {
-				console.log(OutputAvg);
-				console.log(JSON.stringify(OutputAvg));
+		var databaseUrl = "BllipRatings";
+		var collections = ["a70ca2571e1fd4fd79e44c4024995bbe9"]
+		var db = require("mongojs").connect(databaseUrl, collections);
+		db.a70ca2571e1fd4fd79e44c4024995bbe9.find({avg: 1}, {_id: 0}, function(err, a70ca2571e1fd4fd79e44c4024995bbe9){
+			if( err || !a70ca2571e1fd4fd79e44c4024995bbe9) console.log("No average found");
+			else a70ca2571e1fd4fd79e44c4024995bbe9.forEach( function(femaleUser) {
+				console.log(femaleUser);
+				console.log(JSON.stringify(femaleUser));
 				response.writeHead(200, {"Content-Type": "text/plain"});
 		                response.write(JSON.stringify(femaleUser));
 				response.end();
 
 			});
 		});
+//		response.writeHead(200, {"Content-Type": "text/plain"});
+//		response.write("Hello World");
+//		response.end();
 	}
 
 	http.createServer(onRequest).listen(8080);
